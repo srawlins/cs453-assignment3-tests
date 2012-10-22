c83b9b466e0cebe11f55239240f321c22eb9b44f
Samuel R Rawlins <srawlins@cs.arizona.edu>
2012-10-22 16:17:04 -0700

    Not totally finished with 3.1-2 specs

--- go-test-now.sh
+++ go-test-now.sh
@@ -71,7 +71,16 @@ test_stdout () {
 
 test_error_lines () {
   #if diff <($program < ${test_file} 2>&1 >/dev/null |sed 's/^\([0-9]\+\).*/\1/;s/.*[^0-9]\([0-9]\+\).*/\1/') ${expected_errors_file} >/dev/null
-  if diff <($program < ${test_file} 2>&1 >/dev/null |sed 's/^\([0-9]\+\).*/\1/;s/[^0-9]\+\([0-9]\+\).*/\1/' |uniq) ${expected_errors_file} >/dev/null
+  if grep "Segmentation fault" <(sh -c "$program < ${test_file}" 2>&1 >/dev/null) >/dev/null
+  then
+    let failure_count++
+    echo -en "\033[31mF\033[0m"
+
+    space_out_verbose_file
+    echo -e "\033[31mFAILURE ${failure_count}:\033[0m ${test_name}" >> $verbose_file
+    echo "    The expected stderr (\`$program < ${test_file} 2>&1 >/dev/null\`), shows a Segfault:" >> $verbose_file
+    sh -c "$program < ${test_file}" 2>>$verbose_file >/dev/null
+  elif diff <($program < ${test_file} 2>&1 >/dev/null |sed 's/^\([0-9]\+\).*/\1/;s/[^0-9]\+\([0-9]\+\).*/\1/' |uniq) ${expected_errors_file} >/dev/null
   then
     let success_count++
     echo -en "\033[32m.\033[0m"
